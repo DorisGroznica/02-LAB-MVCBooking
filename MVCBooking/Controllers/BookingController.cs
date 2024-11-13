6ca1b23c590647a6886e0b9b540f34a9ab1aef1d
Doris Groznica <dogroznica@vsite.hr>
2024-11-13 23:53:57 +0100

    Lab 2

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> hotelBookings = new();
+        public IActionResult Index()
+        {
+            return View(hotelBookings);
+        }
+        
+        public IActionResult Create()
+        {
+            HotelBooking booking = new HotelBooking();
+            return View(booking);
+        }
+
+        public IActionResult CreateBooking(HotelBooking booking)
+        {
+            hotelBookings.Add(booking);
+            return RedirectToAction("Index");
+        }
+    }
+}
